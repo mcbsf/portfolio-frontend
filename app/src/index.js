5b2fb8cb10144e5fdc02b4bd21d9547cfda4ebbb
mcbsf <mariocbsf@gmail.com>
2023-05-24 19:20:39 +0000

    remove unused component

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import Portfolio from './portfolio/Portfolio';
 import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.css';
