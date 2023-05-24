6087a55362c840fd15c70f1552d31af30916f7b6
mcbsf <mariocbsf@gmail.com>
2023-05-24 21:08:37 +0000

    set proper header order; set better header heigh

--- app/src/header/Header.js
+++ app/src/header/Header.js
@@ -7,10 +7,10 @@ const Header_Like = () => {
         <header className="portfolio-header">
             
             <div className="portfolio-header-right">
-                <a href="#projects">Projects</a>
                 <a href="#professional-experience">Professional Experience</a>
                 <a href="#academic-experience">Academic Experience</a>
                 <a href="#scientific-research">Scientific Research</a>
+                <a href="#projects">Projects</a>
                 <a href="#volunteering">Volunteering</a>
             </div>
         </header>
