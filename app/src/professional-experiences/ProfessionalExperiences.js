0202ce36f1375ecab9602a02379855646b31d0b8
mcbsf <mariocbsf@gmail.com>
2023-05-26 21:17:53 +0000

    refactored prof experiences, isolated data structure from react component logic

--- app/src/professional-experiences/ProfessionalExperiences.js
+++ app/src/professional-experiences/ProfessionalExperiences.js
@@ -1,140 +1,9 @@
 import React, { useState, useRef } from 'react';
 import { Button, Row, List } from 'antd';
 import './ProfessionalExperiences.css';
+import { ProfessionalExperiencesDataStructure } from './ProfessionalExperiencesDataStructure'
 function ProfessionalExperiences() {
-    const professionalExperiences = [
-        {
-            company: "Creditas",
-            position: "Senior Software Engineer",
-            duration: "AUG 2022 - PRESENT",
-            hardSkills: [
-                { 'title': 'python', 'description': 'flask, pandas, pytest' },
-                { 'title': 'javascript', 'description': 'react' },
-                { 'title': 'terraform', 'description': '' },
-                { 'title': 'aws', 'description': '' },
-                { 'title': 'gcp', 'description': '' },
-                { 'title': 'circleci', 'description': '' },
-                { 'title': 'postgres', 'description': '' },
-                { 'title': 'sqs', 'description': '' },
-                { 'title': 'metabase', 'description': '' },
-                { 'title': 'kibana', 'description': '' },
-                { 'title': 'prometheus', 'description': '' }
-            ],
-            responsibilities: [
-                "Stakeholders management to create an efficient backlog",
-                "Integrations between People fintech systems with ETL processes",
-                "Do automations to generate efficiency on People processes"
-            ],
-        },
-        {
-            company: "Realtask",
-            position: "Senior Software Engineer",
-            duration: "JAN 2022 - AUG 2022",
-            hardSkills: [
-                {
-                    'title': "python(fastapi, pandas, pytest, pyspark)",
-                    'description': ''
-                },
-                {
-                    'title': "javascript(react)",
-                    'description': ''
-                },
-                {
-                    'title': "C#(.NET)",
-                    'description': ''
-                },
-                {
-                    'title': "cloudformation",
-                    'description': ''
-                },
-                {
-                    'title': "aws",
-                    'description': ''
-                },
-                {
-                    'title': "azure devops",
-                    'description': ''
-                },
-                {
-                    'title': "jenkins",
-                    'description': ''
-                },
-                {
-                    'title': "postgres",
-                    'description': ''
-                },
-                {
-                    'title': "dynamo",
-                    'description': ''
-                },
-                {
-                    'title': "sqs",
-                    'description': ''
-                },
-                {
-                    'title': "kafka",
-                    'description': ''
-                },
-                {
-                    'title': "athena",
-                    'description': ''
-                },
-                "glue",
-                "cloudwatch"
-            ],
-            responsibilities: [
-                "Worked in a project at BTG Pactual, one of the biggest investment banks in LATAM, as a Software Engineer focused on Data Engineering",
-                "Cross project to segment and map costs to help interested areas"
-            ],
-        },
-        {
-            company: "Avantia",
-            position: "Software Developer",
-            duration: "JAN 2021 - JAN 2022",
-            hardSkills: [
-                "python(opencv, tornado)",
-                "typescript(angular)",
-                "aws",
-                "azure devops",
-                "postgres",
-                "grey log"
-            ],
-            responsibilities: [
-                "Developed and maintained video monitoring systems to manage Artificial Intelligence services signals, focused on security"
-            ],
-        },
-        {
-            company: "Euromecantil",
-            position: "Software Engineer",
-            duration: "JUN 2019 - JAN 2021",
-            hardSkills: [
-                { 'title': 'python', 'description': 'opencv, tornado' },
-                { 'title': 'typescript', 'description': 'angular' },
-                { 'title': 'aws', 'description': '' },
-                { 'title': 'azure devops', 'description': '' },
-                { 'title': 'postgres', 'description': '' },
-                { 'title': 'grey log', 'description': '' }
-            ],
-            responsibilities: [
-                "Started as a salesman and finished as IT manager in this fintech startup",
-                "Planned the architecture change from monolith to microservices",
-                "Managed a 3 person team to build CRM, ERP, and auditing systems"
-            ],
-        },
-        {
-            company: "VTB Solutions",
-            position: "Intern Software Developer",
-            duration: "FEB 2019 - JUN 2019",
-            hardSkills: [
-                { 'title': 'C#', 'description': '.NET' },
-                { 'title': 'Power BI', 'description': '' },
-                { 'title': 'MongoDB', 'description': '' }
-            ],
-            responsibilities: [
-                "Software Developer in software factory to build ERP and CRM for several clients"
-            ],
-        },
-    ];
+    const professionalExperiences = ProfessionalExperiencesDataStructure;
 
     const [hide, setHide] = useState(Array(professionalExperiences.length).fill(false));
 
@@ -158,7 +27,6 @@ function ProfessionalExperiences() {
                 detailsRef.scrollIntoView({ behavior: 'smooth' });
             }
         }, 10);
-
     };
     return (
         <div>
@@ -189,26 +57,22 @@ function ProfessionalExperiences() {
                                 </div>
 
                             </Row>
-
                             <div className='professional-exp-details' ref={(ref) => (detailsRefs.current[index] = ref)}>
-
                                 <div className='hardskills'>
+                                    <List
+                                        header={<div>HardSkills</div>}
+                                        itemLayout="horizontal"
+                                        dataSource={experience.hardSkills}
+                                        renderItem={(hardskill, index) => (
+                                            <List.Item>
+                                                <List.Item.Meta
 
-                                        <List
-                                            header={<div>HardSkills</div>}
-                                            itemLayout="horizontal"
-                                            dataSource={experience.hardSkills}
-                                            renderItem={(hardskill, index) => (
-                                                <List.Item>
-                                                    <List.Item.Meta
-
-                                                        title={<div className="hardskill-title" style={{ marginBottom: 0 }}>{hardskill.title}</div>}
-                                                        description={<div className="hardskill-description" style={{ marginLeft: '10px' }}>{hardskill.description}</div>}
-                                                    />
-                                                </List.Item>
-                                            )}
-                                        />
-
+                                                    title={<div className="hardskill-title" style={{ marginBottom: 0 }}>{hardskill.title}</div>}
+                                                    description={<div className="hardskill-description" style={{ marginLeft: '10px' }}>{hardskill.description}</div>}
+                                                />
+                                            </List.Item>
+                                        )}
+                                    />
                                 </div>
                                 <div className='responsabilities'></div>
                                 <Button
