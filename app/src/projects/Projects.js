4046cea265745c9b62813240b864186207b69e1d
mcbsf <mariocbsf@gmail.com>
2023-05-30 19:49:04 +0000

    removing unused imports

--- app/src/projects/Projects.js
+++ app/src/projects/Projects.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import { SocialIcon } from 'react-social-icons';
 import './Projects.css';
 import { ProjectsDataStructure } from './ProjectsDataStructure'
